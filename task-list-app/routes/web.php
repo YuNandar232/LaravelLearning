19c992abbe8c29308bfa22c350441dcdb6d9e7b4
Yu Nandar <135571225+YuNandar232@users.noreply.github.com>
2025-01-10 16:29:27 +0630

    tasks-crud-date

--- task-list-app/routes/web.php
+++ task-list-app/routes/web.php
@@ -1,7 +1,49 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks,
+    ]);
 });
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+})->name('tasks.store');
+
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
+})->name('tasks.destroy');
